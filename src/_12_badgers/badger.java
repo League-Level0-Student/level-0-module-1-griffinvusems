2e3b351e56a6762407d71162bab079179edbaceb
vu.griffin <vu.griffin@SEMS-259-31.SEMS259.LAB>
2019-12-18 16:24:14 -0800

    good job

---
+++ src/_12_badgers/badger.java
@@ -0,0 +1,41 @@
+package _12_badgers;
+
+public class badger { 
+	public static void main(String[] args) {
+		for (int k = 0; k < 4; k++) {
+		
+		for (int i = 0; i < 4; i++) {
+		
+		for (int j = 0; j < 3; j++) {
+			System.out.print("Badger");
+			if (j==1||j==0) {
+				System.out.print(",");
+			}
+		}
+		System.out.println();
+
+	}
+		for (int j = 0; j < 2; j++) {
+			System.out.print("Mushroom");
+			if (j==0||j==0) {
+				System.out.print(",");
+				
+		}
+	}
+		
+				
+	
+		System.out.println();
+		}
+		for (int z = 0; z < 4; z++) {
+			System.out.print("Snake");
+			if (z==0||z==0) {
+				System.out.print(",");	
+				System.out.println();
+		}
+			
+}
+		System.out.println();
+}
+}
+
